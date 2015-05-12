c9bafcfa5a1598175796cdd19fde3769ee2fa828
Julia Angley <jangley@Julias-MacBook-Air.local>
2015-05-11 22:15:08 -0400

    document attempts to combine two graphs... partially working, may be issue with token?

---
+++ combine.js
@@ -0,0 +1,219 @@
+var tessel = require('tessel');
+var myPin = tessel.port['GPIO'].pin['G3'];
+var myPin2 = tessel.port['GPIO'].pin['G4'];
+
+var myUser = "jangley1";
+var myApi = "xojqlgubes";
+var myToken = 'jg86i7eezv';
+
+function getTitle() {
+    var myDate = new Date()
+    var splitDate = myDate.toString().split(' ')
+    var titleMonth = splitDate[1]
+    var titleDay = splitDate[2]
+    var titleYear = splitDate[3]
+    var titleDate = titleMonth + ' ' + titleDay + ", " + titleYear;
+    return titleDate;
+}
+
+var refresh = 30 * 1000;
+var getMyDataFunction = function(stream) {
+    return function() {
+        var myDate = new Date() 
+        var time = (myDate.getHours() -4) + ":" + ('0'+myDate.getMinutes()).slice(-2);
+        var yvalue = null;
+        var label = null;
+        if (myPin.read()==0) {
+            yvalue = 2,
+            label = 'Standing'
+        }
+        else if (myPin2.read()==0) {
+            yvalue = 0
+            label = 'Away'
+        }
+        else if (myPin.read()==1 && myPin2.read()==1) {
+            yvalue = .5
+            label = 'Sitting'      
+          }
+        var data = { 'x': time , 'y': yvalue, 'text': label};
+        console.log("Sending", JSON.stringify(data));
+        stream.write(JSON.stringify(data) + '\n');
+    };
+};
+
+
+function sendData(token, refreshRate) {
+    var plotly = require('plotly')(myUser, myApi);
+    var data = {
+        'x': [] // empty arrays since we will be streaming our data to into these arrays
+            ,
+        'y': [],
+        'type': 'bar',
+        'text': [],
+        'marker': {
+            'color': '#00005C'
+        },
+        stream: {
+            "token": token
+            //"maxpoints": 500
+        }
+    }
+
+    var layout = {
+        "filename": getTitle(),
+        "fileopt": "overwrite",
+        "layout": {
+            autosize:true,
+            "title": getTitle(),
+            bargap: 0, 
+            "yaxis":{
+                type:"linear",
+                showgrid:false,
+                domain:[
+                  0,
+                  1
+                ],
+                showticklabels:false,
+                range:[
+                  0,
+                  2
+                ],
+                autorange:false
+                },
+            "xaxis":{
+              type:"category",
+              autorange:true
+            }
+        },
+        "world_readable": true
+    };
+
+
+
+    plotly.plot(data, layout, function(err, msg) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(msg);
+
+        var myStream = plotly.stream(token, function(err, res) {
+            if (err) return console.log(err);
+        });
+
+        // Loop forever
+        setInterval(getMyDataFunction(myStream), refreshRate);
+    });
+};
+
+sendData(myToken, refresh);
+
+var myToken2 = 'yzy1b8lad4';
+
+var sitarray = [];
+var sitinc = '1';
+
+var standarray = [];
+var standinc = '1';
+
+var awayarray = [];
+var awayinc = '1';
+
+setInterval(function () {
+    if (myPin.read()==0) {
+    standarray.push(standinc);
+    }
+}, refresh);
+
+
+setInterval(function () {
+    if (myPin2.read()==0) {
+    awayarray.push(awayinc);
+    }
+}, refresh);
+
+
+setInterval(function () {
+    if (myPin.read()==1 && myPin2.read()==1) {
+    sitarray.push(sitinc);
+    }
+}, refresh);
+
+var getMyDataFunction2 = function(stream) {
+    return function() {
+        var xvalue = null;
+        var label = null;
+        var yvalue = null;
+        if (myPin.read()==0) {
+            xvalue = 'Standing',
+            label = 'Standing',
+            yvalue = (standarray.length)/2
+        }
+        else if (myPin2.read()==0) {
+            xvalue = 'Away',
+            label = 'Away',
+            yvalue = (awayarray.length)/2
+        }
+        else if (myPin.read()==1 && myPin2.read()==1) {
+            xvalue = 'Sitting',
+            label = 'Sitting',
+            yvalue = (sitarray.length)/2    
+          }
+        var data = { 'x': xvalue, 'y': yvalue, 'name': label};
+        // console.log('Away: ' + awayarray);
+        // console.log('Sit: ' + sitarray);
+        // console.log('Stand: ' + standarray);
+        console.log("Sending", JSON.stringify(data));
+        stream.write(JSON.stringify(data) + '\n');
+    };
+};
+
+
+function sendData2(token, refreshRate) {
+    var plotly = require('plotly')(myUser, myApi);
+    var data = {
+        'x': [] // empty arrays since we will be streaming our data to into these arrays
+            ,
+        'y': [],
+        'name': [],
+        'type': 'bar',
+        'marker': {
+            'color': '#666666'
+        },
+        stream: {
+            "token": token
+            //"maxpoints": 500
+        }
+    }
+
+    var layout = {
+        "filename": 'Time in Each Position ' + getTitle(),
+        "fileopt": "overwrite",
+        "layout": {
+            autosize:true,
+            "yaxis":{
+                title:"minutes",
+                showgrid:false
+                }
+        },
+        "world_readable": true,
+        title:"Time in Each Position"
+    };
+
+
+
+    plotly.plot(data, layout, function(err, msg) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(msg);
+
+        var myStream = plotly.stream(token, function(err, res) {
+            if (err) return console.log(err);
+        });
+
+        // Loop forever
+        setInterval(getMyDataFunction2(myStream), refreshRate);
+    });
+};
+
+sendData2(myToken2, refresh);
